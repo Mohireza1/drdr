3ddea9db84dc4d8a2ce3a36683210d0cdb02c9c4
mohireza <mohireza50@gmail.com>
2026-05-13 21:53:50 +0330

    open animation

--- app/home/components/BurgerMenu.tsx
+++ app/home/components/BurgerMenu.tsx
@@ -6,6 +6,7 @@ import { CgClose } from "react-icons/cg";
 import { CiInstagram, CiLinkedin } from "react-icons/ci";
 import { GrChat } from "react-icons/gr";
 import { LiaTelegramPlane } from "react-icons/lia";
+import { AnimatePresence, motion } from "framer-motion";
 
 interface BurgerMenuProps {
   isOpen: boolean;
@@ -13,69 +14,73 @@ interface BurgerMenuProps {
 }
 
 export default function BurgerMenu({ isOpen, setIsOpen }: BurgerMenuProps) {
-  if (!isOpen) return null;
-
   return (
-    <div className="fixed inset-0 z-50">
-      <div
-        className="absolute inset-0 bg-gray-200/60 backdrop-blur-xs"
-        onClick={() => setIsOpen(false)}
-      />
-
-      <div
-        dir="rtl"
-        onClick={(e) => e.stopPropagation()}
-        className="absolute top-0 right-0 w-65 h-screen bg-white shadow-gray-400/70 shadow-lg flex flex-col"
-      >
-        <div
-          dir="ltr"
-          className="grid grid-cols-[1fr_4fr] grid-rows-[auto_auto] gap-x-3 gap-y-6 mt-20 mb-10 px-5 items-center"
-        >
-          <button
-            className="justify-self-start text-gray-600"
+    <AnimatePresence>
+      {isOpen && (
+        <div className="fixed inset-0 z-50">
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.08 }}
+            className="absolute inset-0 bg-gray-200/60 backdrop-blur-xs"
             onClick={() => setIsOpen(false)}
+          />
+          <motion.div
+            dir="rtl"
+            onClick={(e) => e.stopPropagation()}
+            initial={{ right: -270 }}
+            animate={{ right: 0 }}
+            exit={{ right: -270 }}
+            transition={{ type: "spring", stiffness: 180, damping: 20 }}
+            className="absolute top-0 right-0 w-65 h-screen bg-white shadow-gray-400/70 shadow-lg flex flex-col"
           >
-            <CgClose size={25} />
-          </button>
-
-          <Image alt="logo" src="/logo.png" width={131} height={28} />
-
-          <button
-            className="col-span-2 flex justify-center gap-4 w-full p-2
-          border border-blue-600 text-blue-600 rounded-md 
-          hover:bg-blue-600 hover:text-white transition-all duration-150"
-          >
-            ورود/عضویت
-            <BiUser size={20} />
-          </button>
+            <div
+              dir="ltr"
+              className="grid grid-cols-[1fr_4fr] grid-rows-[auto_auto] gap-x-3 gap-y-6 mt-20 mb-10 px-5 items-center"
+            >
+              <button
+                className="justify-self-start text-gray-600"
+                onClick={() => setIsOpen(false)}
+              >
+                <CgClose size={25} />
+              </button>
+              <Image alt="logo" src="/logo.png" width={131} height={28} />
+              <button
+                className="col-span-2 flex justify-center gap-4 w-full p-2
+            border border-blue-600 text-blue-600 rounded-md
+            hover:bg-blue-600 hover:text-white transition-all duration-150"
+              >
+                ورود/عضویت
+                <BiUser size={20} />
+              </button>
+            </div>
+            <div
+              className="flex-1 mr-4 flex flex-col overflow-auto text-gray-700
+             divide-y divide-gray-200
+             **:py-3 *:flex *:items-center"
+            >
+              <div>خانه</div>
+              <div>رزرو نوبت</div>
+              <div>درباره ما</div>
+              <div>تماس با ما</div>
+            </div>
+            <button
+              className="flex gap-2 justify-center items-center mt-6 mx-3 py-3 px-4 text-sm font-bold border bg-blue-600 text-white rounded-md
+            hover:bg-blue-600 hover:text-white transition duration-150
+            active:bg-blue-900 active:text-gray-100"
+            >
+              <GrChat />
+              <p>تماس با پشتیبانی</p>
+            </button>
+            <div className="flex justify-around my-6 text-2xl text-sky-600 transition duration-150">
+              <CiInstagram />
+              <CiLinkedin />
+              <LiaTelegramPlane />
+            </div>
+          </motion.div>
         </div>
-
-        <div
-          className="flex-1 mr-4 flex flex-col overflow-auto text-gray-700
-           divide-y divide-gray-200
-           **:py-3 *:flex *:items-center"
-        >
-          <div>خانه</div>
-          <div>رزرو نوبت</div>
-          <div>درباره ما</div>
-          <div>تماس با ما</div>
-        </div>
-
-        <button
-          className="flex gap-2 justify-center items-center mt-6 mx-3 py-3 px-4 text-sm font-bold border bg-blue-600 text-white rounded-md
-          hover:bg-blue-600 hover:text-white transition duration-150
-          active:bg-blue-900 active:text-gray-100"
-        >
-          <GrChat />
-          <p>تماس با پشتیبانی</p>
-        </button>
-
-        <div className="flex justify-around my-6 text-2xl text-sky-600 transition duration-150">
-          <CiInstagram />
-          <CiLinkedin />
-          <LiaTelegramPlane />
-        </div>
-      </div>
-    </div>
+      )}
+    </AnimatePresence>
   );
 }
