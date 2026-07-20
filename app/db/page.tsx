2e07a6c286ae097bad7f096dff64dc0269f8017b
mohireza <mohireza50@gmail.com>
2026-07-20 22:22:18 +0330

    add qol changes

--- app/db/page.tsx
+++ app/db/page.tsx
@@ -1,8 +1,12 @@
 import { Suspense } from "react";
 import { getComments } from "../home/lib/queries";
+import { cacheLife, cacheTag } from "next/cache";
 
 // Separate component that fetches data
 async function CommentsList() {
+  "use cache";
+  cacheLife({ stale: 604800, revalidate: 6048000 });
+  cacheTag("comments");
   const data = await getComments(); // Await happens inside Suspense boundary
 
   return (
---
+++ scripts/seedComments.ts
@@ -0,0 +1,5 @@
+import sql from "@/db/client";
+
+const seedComments = async () => {
+    
+}
